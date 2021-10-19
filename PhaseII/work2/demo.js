faaeb9a5b1f5e69df068d942c0f27108ac9ff0d9
13839589657 <2114860175@qq.com>
2021-10-19 22:19:02 +0800

    feat:README

--- PhaseII/work2/demo.js
+++ PhaseII/work2/demo.js
@@ -1,27 +1,26 @@
 // let arr = [12, 17, 9, 21, 3, 6, 25];
-// // 冒泡排序;
-// for (let i = 0; i < arr.length; i++) {
-//   for (let j = i + 1; j < arr.length; j++) {
-//     if (arr[i] > arr[j]) {
-//       [arr[i], arr[j]] = [arr[j], arr[i]];
-//     }
-//   }
-// }
-// console.log(arr);
+// 冒泡排序;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] > arr[j]) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+}
+console.log(arr);
 
-// // 插入排序
-// for (let i = 1; i < arr.length; i++) {
-//   let key = arr[i];
-//   let j = i - 1;
-//   while (j >= 0 && arr[j] > key) {
-//     arr[j + 1] = arr[j];
-//     j--;
-//   }
-//   arr[j + 1] = key;
-// }
-// console.log(arr);
+// 插入排序
+for (let i = 1; i < arr.length; i++) {
+  let key = arr[i];
+  let j = i - 1;
+  while (j >= 0 && arr[j] > key) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = key;
+}
+console.log(arr);
 
-<<<<<<< HEAD
 // 选择排序;
 let arr = [12, 17, 9, 21, 3, 6, 25];
 for (var i = 0; i < arr.length; i++) {
@@ -36,7 +35,6 @@ for (var i = 0; i < arr.length; i++) {
 console.log(arr);
 
 // 快速排序
-
 function quick(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -54,44 +52,12 @@ function quick(arr) {
   return quick(left).concat(num, quick(right));
 }
 console.log(quick(arr));
-=======
-// // 选择排序;
-// for (var i = 0; i < arr.length; i++) {
-//   var key = i;
-//   for (var j = i + 1; j < arr.length; j++) {
-//     if (arr[j] < arr[key]) {
-//       key = j;
-//     }
-//   }
-//   [arr[i], arr[key]] = [arr[key], arr[i]];
-// }
-// console.log(arr);
-
-// // 快速排序
-// function quick(arr) {
-//   if (arr.length <= 1) {
-//     return arr;
-//   }
-//   var left = [];
-//   var right = [];
-//   var num = arr.splice(Math.floor(arr.length / 2), 1);
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] < num) {
-//       left.push(arr[i]);
-//     } else {
-//       right.push(arr[i]);
-//     }
-//   }
-//   return quick(left).concat(num, quick(right));
-// }
-// console.log(quick(arr));
->>>>>>> 853a92d85a35ccf5feb43e4ccb195046fadc96b7
 
 // 桶排序
-let newArr = [2, 9, 3, 8, 4, 0]
-let buk = []
+let newArr = [2, 9, 3, 8, 4, 0];
+let buk = [];
 for (let j = 0; j < newArr.length; j++) {
-  buk[newArr[j]] = "abc"
+  buk[newArr[j]] = 'abc';
   // buk[2] = "abc"
   // buk[9] = "abc"
   // buk[3] = "abc"
@@ -100,10 +66,10 @@ for (let j = 0; j < newArr.length; j++) {
   // buk[0] = "abc"
   // buk = ["abc", undefined, "abc", "abc", "abc", undefined, undefined, undefined, "abc", "abc"]
 }
-let arr = []
+let arr = [];
 for (let i = 0; i < buk.length; i++) {
   if (buk[i]) {
-    arr.push(i)
+    arr.push(i);
   }
 }
-console.log(arr)
+console.log(arr);
---
+++ README.md
@@ -0,0 +1,29 @@
+#### 10-12：
+    if、switch、for、while、do while语法复习
+    this指向学+练
+    let、const变量学+练
+    Phasel work3页面补充
+#### 10-13：
+    学：javascript错误try、catch、throw、finally
+    正则表达式
+    call、apply、bind区别
+    Promise
+    复习：flex
+
+    问题：Promise懂了又好像没懂，只是会了基本的用法
+
+#### 10-14：
+    work4、work5效果实现
+
+#### 10-15：
+    Phase|| work1练习
+    js基础视频复习、排序学习
+
+#### 10-18:
+    排序练习
+    问题：桶排序算法搞不清楚
+
+#### 10-19:
+    面向对象初步了解，代码练习
+    面向过程：将问题分解成步骤一步一步去实现执行
+    面向对象：将面向过程的代码封装起来去调用
