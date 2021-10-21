a67ed05b1e682c02b05a4fed80a3cb458b8567f4
13839589657 <2114860175@qq.com>
2021-10-21 21:43:42 +0800

    feat:work4

---
+++ PhaseII/work4/position.js
@@ -0,0 +1,19 @@
+function mouseUp(e, myBox) {
+  var left = e.clientX - myBox.offsetWidth / 2;
+  var top = e.clientY - myBox.offsetWidth / 2;
+  if (left <= 0) {
+    left = 0;
+  } else if (left >= document.documentElement.clientWidth - myBox.offsetWidth) {
+    left = document.documentElement.clientWidth - myBox.offsetWidth;
+  } else if (top <= 0) {
+    top = 0;
+  } else if (top >= document.documentElement.clientHeight - myBox.offsetWidth) {
+    top = document.documentElement.clientHeight - myBox.offsetWidth;
+  }
+  myBox.style.left = left + 'px';
+  myBox.style.top = top + 'px';
+  position.push({
+    X: left,
+    Y: top,
+  });
+}
